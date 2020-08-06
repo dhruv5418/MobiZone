25b06fc6c2abc9bc24c8c81c0f0f70f614a041d0
sarbjit kaur <nishuuukaur12@gmail.com>
2020-08-06 14:14:03 -0400

    Click listener added for AddressActivity added .

--- app/src/main/java/com/example/mobizone/view/AddressActivity.java
+++ app/src/main/java/com/example/mobizone/view/AddressActivity.java
@@ -2,15 +2,31 @@ package com.example.mobizone.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mobizone.R;
 
-public class AddressActivity extends AppCompatActivity {
+public class AddressActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btn_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
+        btn_add=findViewById(R.id.btn_addNxt);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+
+            case R.id.btn_addNxt:intent=new Intent(AddressActivity.this,PaymentActivity.class);
+                                 startActivity(intent);
+                                 break;
+        }
     }
 }
