71dcfaa4583bb9c13c4b6f622350a4ef70b497ce
gakha <gakhartanvi@gmail.com>
2020-08-06 17:18:33 -0400

    ThankyouActivity.java and layout file code done.

--- app/src/main/java/com/example/mobizone/ThankyouActivity.java
+++ app/src/main/java/com/example/mobizone/ThankyouActivity.java
@@ -2,13 +2,27 @@ package com.example.mobizone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mobizone.view.DashActivity;
 
 public class ThankyouActivity extends AppCompatActivity {
 
+    Button btn_con;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thankyou);
+        btn_con=findViewById(R.id.btn_continue);
+        btn_con.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ThankyouActivity.this, DashActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
