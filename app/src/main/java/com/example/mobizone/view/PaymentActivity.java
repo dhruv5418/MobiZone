c9d69375cc880fbc41e673c456b1cfc2df823ce3
Kamal <saikamaljit181294@gmail.com>
2020-08-06 15:01:21 -0400

    ThankyouActivity added to show after placing the order.

--- app/src/main/java/com/example/mobizone/view/PaymentActivity.java
+++ app/src/main/java/com/example/mobizone/view/PaymentActivity.java
@@ -2,15 +2,28 @@ package com.example.mobizone.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mobizone.R;
+import com.example.mobizone.ThankyouActivity;
 
 public class PaymentActivity extends AppCompatActivity {
 
+    Button btn_pay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        btn_pay=findViewById(R.id.btn_pay);
+        btn_pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(PaymentActivity.this, ThankyouActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
