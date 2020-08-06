6aa560a274fbd1fc7984e3d96d4b212e244ab557
akshay <akshay9varma@gmail.com>
2020-08-06 12:12:36 -0400

    Java code of onClick for all buttons added in login and register.

--- app/src/main/java/com/example/mobizone/LoginFragment.java
+++ app/src/main/java/com/example/mobizone/LoginFragment.java
@@ -1,58 +1,45 @@
 package com.example.mobizone;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class LoginFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    Button btn_log;
+    TextView txt_reg,txt_forgot;
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_log=view.findViewById(R.id.btn_log);
+        txt_reg=view.findViewById(R.id.txt_logR);
+        txt_forgot=view.findViewById(R.id.txt_forgot);
+        txt_reg.setOnClickListener(this);
+        btn_log.setOnClickListener(this);
     }
 
     @Override
@@ -60,5 +47,23 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+    @Override
+    public void onClick(View view) {
+        NavController navController= Navigation.findNavController(getActivity(),R.id.nav_host_login);
+        switch (view.getId()){
+            case R.id.btn_log:
+                Intent intent =new Intent(getActivity().getApplicationContext(),DashActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.txt_logR:
+                navController.navigate(R.id.registerFragment);
+                break;
+            case R.id.txt_forgot:
+                navController.navigate(R.id.forgotpassFragment);
+                break;
+
+
+        }
     }
 }
