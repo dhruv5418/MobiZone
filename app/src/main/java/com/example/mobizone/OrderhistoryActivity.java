c9ce5f6fe8e18390afbe476885be0b43b78e02f7
akshay <akshay9varma@gmail.com>
2020-08-06 14:50:20 -0400

    Navigation click listener added for OrderhistoryActivity toolbar

--- app/src/main/java/com/example/mobizone/OrderhistoryActivity.java
+++ app/src/main/java/com/example/mobizone/OrderhistoryActivity.java
@@ -1,14 +1,24 @@
 package com.example.mobizone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class OrderhistoryActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orderhistory);
+        toolbar=findViewById(R.id.toolbar_orderHistory);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
