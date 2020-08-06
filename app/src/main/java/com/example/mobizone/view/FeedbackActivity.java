18a1609b937bcffc96d10226f761b5950626674d
gakha <gakhartanvi@gmail.com>
2020-08-06 14:31:20 -0400

    In FeedbackActivity.java code for email intent added to send feedback via mail.

--- app/src/main/java/com/example/mobizone/view/FeedbackActivity.java
+++ app/src/main/java/com/example/mobizone/view/FeedbackActivity.java
@@ -2,15 +2,54 @@ package com.example.mobizone.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.mobizone.R;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    Button btn_send;
+    EditText txt_message;
+    String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        btn_send=findViewById(R.id.btn_feedBack);
+        txt_message=findViewById(R.id.edt_feedBack);
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message=txt_message.getText().toString();
+                sendFeedback();
+            }
+        });
+    }
+
+    private void sendFeedback() {
+        String[] TO = {"dhruvj5418@gmail.com","gakhartanvi@gmail.com"};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback for Mobizone");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            //finish();
+            //Log.i("Finished sending email...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            //Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
