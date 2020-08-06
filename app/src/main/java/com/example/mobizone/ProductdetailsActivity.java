4a4177271bb439b597db9089bf667c3b2436b822
Dhruv <dhruvj5418@gmail.com>
2020-08-06 10:18:28 -0400

    Product details activity added with all widgets.

---
+++ app/src/main/java/com/example/mobizone/ProductdetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobizone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductdetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productdetails);
+    }
+}
