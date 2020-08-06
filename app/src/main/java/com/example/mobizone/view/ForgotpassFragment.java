6e727e08bf9576321e6bb9201720eb49690111b0
sarbjit kaur <nishuuukaur12@gmail.com>
2020-08-06 14:07:53 -0400

    Click listener added for ForgotpassFragment .

--- app/src/main/java/com/example/mobizone/view/ForgotpassFragment.java
+++ app/src/main/java/com/example/mobizone/view/ForgotpassFragment.java
@@ -2,59 +2,43 @@ package com.example.mobizone.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mobizone.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ForgotpassFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ForgotpassFragment extends Fragment {
+public class ForgotpassFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+Button btn_send;
+TextView txt_login;
 
     public ForgotpassFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForgotpassFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ForgotpassFragment newInstance(String param1, String param2) {
-        ForgotpassFragment fragment = new ForgotpassFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_send=view.findViewById(R.id.btn_frgtPass);
+        txt_login=view.findViewById(R.id.txt_frgtLogin);
+        btn_send.setOnClickListener(this);
+        txt_login.setOnClickListener(this);
     }
 
     @Override
@@ -62,5 +46,18 @@ public class ForgotpassFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_forgotpass, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        NavController navController= Navigation.findNavController(getActivity(),R.id.nav_host_login);
+        switch (view.getId()){
+            case R.id.btn_frgtPass:navController.navigate(R.id.loginFragment);
+                                   break;
+            case R.id.txt_frgtLogin:navController.navigate(R.id.loginFragment);
+                                    break;
+
+        }
+
     }
 }
