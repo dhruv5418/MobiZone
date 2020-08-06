3e71a8356c0f6587867eac1b39b28338ebc8640f
Dhruv <dhruvj5418@gmail.com>
2020-08-06 15:29:25 -0400

    Item Layout for card item added and some addition in ProductdetailsActivity.java file.

--- app/src/main/java/com/example/mobizone/view/AddressActivity.java
+++ app/src/main/java/com/example/mobizone/view/AddressActivity.java
@@ -1,22 +1,33 @@
 package com.example.mobizone.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 import com.example.mobizone.R;
 
 public class AddressActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Toolbar toolbar;
     Button btn_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
         btn_add=findViewById(R.id.btn_addNxt);
+        toolbar=findViewById(R.id.toolbar_address);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        btn_add.setOnClickListener(this);
     }
 
     @Override
--- app/src/main/java/com/example/mobizone/view/ProductdetailsActivity.java
+++ app/src/main/java/com/example/mobizone/view/ProductdetailsActivity.java
@@ -2,15 +2,64 @@ package com.example.mobizone.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.mobizone.R;
 
-public class ProductdetailsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ProductdetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_addCart,btn_orderNow;
+    Spinner spinner;
+    ImageView img_backDetails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productdetails);
+        btn_addCart=findViewById(R.id.btn_addCart);
+        btn_orderNow=findViewById(R.id.btn_orderNow);
+        img_backDetails=findViewById(R.id.img_backDetails);
+        img_backDetails.setOnClickListener(this);
+        spinner=findViewById(R.id.spinner);
+        setSpinner();
+        btn_addCart.setOnClickListener(this);
+        btn_orderNow.setOnClickListener(this);
+    }
+
+    private void setSpinner() {
+        List<Integer> categories = new ArrayList<>();
+        for (int i=1;i<=10;i++){
+            categories.add(i);
+        }
+        ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.btn_addCart:
+                Toast.makeText(getApplicationContext(),"Item Added to cart!",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btn_orderNow:
+                Intent intent=new Intent(ProductdetailsActivity.this,AddressActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.img_backDetails:onBackPressed();
+                    break;
+
+        }
+
     }
 }
