6172ccc10e3b263206ff2a79be6de81b8a0352c3
Kamal <saikamaljit181294@gmail.com>
2020-08-26 13:08:35 -0400

    Java code for Forgot Password added in ForgotpassFragment.

--- app/src/main/java/com/example/mobizone/view/ForgotpassFragment.java
+++ app/src/main/java/com/example/mobizone/view/ForgotpassFragment.java
@@ -8,19 +8,28 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobizone.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotpassFragment extends Fragment implements View.OnClickListener {
 
 Button btn_send;
 TextView txt_login;
-
+    private FirebaseAuth auth;
+    private EditText edt_email;
+    NavController navController;
+    String email;
     public ForgotpassFragment() {
         // Required empty public constructor
     }
@@ -29,7 +38,7 @@ TextView txt_login;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        auth=FirebaseAuth.getInstance();
     }
 
     @Override
@@ -37,6 +46,7 @@ TextView txt_login;
         super.onViewCreated(view, savedInstanceState);
         btn_send=view.findViewById(R.id.btn_frgtPass);
         txt_login=view.findViewById(R.id.txt_frgtLogin);
+        edt_email=view.findViewById(R.id.edit_restPassEmail);
         btn_send.setOnClickListener(this);
         txt_login.setOnClickListener(this);
     }
@@ -52,12 +62,32 @@ TextView txt_login;
     public void onClick(View view) {
         NavController navController= Navigation.findNavController(getActivity(),R.id.nav_host_login);
         switch (view.getId()){
-            case R.id.btn_frgtPass:navController.navigate(R.id.loginFragment);
+            case R.id.btn_frgtPass:
+                email=edt_email.getText().toString();
+                if(TextUtils.isEmpty(email)){
+                    edt_email.setError("Please enter email!");
+                    edt_email.requestFocus();
+                }else{
+                    reset(email);
+                }
                                    break;
             case R.id.txt_frgtLogin:navController.navigate(R.id.loginFragment);
                                     break;
 
         }
 
+    }
+
+    private void reset(String email) {
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getActivity().getApplicationContext(),"Email sent successfully!",Toast.LENGTH_LONG).show();
+                    navController= Navigation.findNavController(getActivity(),R.id.nav_host_login);
+                    navController.navigate(R.id.loginFragment);
+                }
+            }
+        });
     }
 }
