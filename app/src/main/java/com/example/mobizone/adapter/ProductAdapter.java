ae7b05d373785efe538e3ae8a58130e3e676d2df
gakha <gakhartanvi@gmail.com>
2020-09-04 21:58:39 -0400

    Product data sent through intent from homeFragment to ProductdetailsActivity.

--- app/src/main/java/com/example/mobizone/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/mobizone/adapter/ProductAdapter.java
@@ -2,6 +2,7 @@ package com.example.mobizone.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,13 +40,45 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final ProductViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ProductViewHolder holder, final int position) {
         Picasso.get().load(productsList.get(position).getImageUrl()).into(holder.prodImage);
         holder.prodName.setText(productsList.get(position).getProductName());
         holder.prodPrice.setText(productsList.get(position).getProductPrice());
         holder.prodMemory.setText(productsList.get(position).getMemory());
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, ProductdetailsActivity.class);
 
+
+                Bundle b = new Bundle();
+                String name=productsList.get(position).getProductName();
+                String company=productsList.get(position).getCompany();
+                String memory=productsList.get(position).getMemory();
+                String price=productsList.get(position).getProductPrice();
+                int id=productsList.get(position).getProductid();
+                String image_detail=productsList.get(position).getImage_detail();
+                String processor=productsList.get(position).getProcessor();
+                String battery=productsList.get(position).getBattery();
+                String frntCam=productsList.get(position).getFrntCam();
+                String bckCam=productsList.get(position).getBckCam();
+                String os=productsList.get(position).getOs();
+                b.putString("Name",name);
+                b.putString("Company",company);
+                b.putString("Memory",memory);
+                b.putString("Price",price);
+                b.putString("Processor",processor);
+                b.putString("Battery",battery);
+                b.putString("Front",frntCam);
+                b.putString("Back",bckCam);
+                b.putString("Os",os);
+                b.putInt("id", id);
+                b.putString("Image", String.valueOf(image_detail));
+                i.putExtras(b);
+                context.startActivity(i);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/mobizone/view/ProductdetailsActivity.java
+++ app/src/main/java/com/example/mobizone/view/ProductdetailsActivity.java
@@ -9,9 +9,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mobizone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,20 +24,58 @@ public class ProductdetailsActivity extends AppCompatActivity implements View.On
 
     Button btn_addCart,btn_orderNow;
     Spinner spinner;
-    ImageView img_backDetails;
+    ImageView img_backDetails,img_product;
+    TextView name,company,price,memory,battery,processor,frnt,bck,os;
+    FirebaseAuth auth;
+    FirebaseFirestore db;
+    String Pname,Pprice,Pqunt,Pimage;
+    int Pid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productdetails);
+        auth=FirebaseAuth.getInstance();
+        db=FirebaseFirestore.getInstance();
         btn_addCart=findViewById(R.id.btn_addCart);
         btn_orderNow=findViewById(R.id.btn_orderNow);
         img_backDetails=findViewById(R.id.img_backDetails);
         img_backDetails.setOnClickListener(this);
+        name=findViewById(R.id.txt_name);
+        company=findViewById(R.id.txt_company);
+        os=findViewById(R.id.txt_os);
+        battery=findViewById(R.id.txt_battery);
+        memory=findViewById(R.id.txt_memory);
+        frnt=findViewById(R.id.txt_frntCam);
+        bck=findViewById(R.id.txt_bckCam);
+        processor=findViewById(R.id.txt_processor);
+        price=findViewById(R.id.txt_price);
+        img_product=findViewById(R.id.img_prod);
+        showData();
         spinner=findViewById(R.id.spinner);
         setSpinner();
         btn_addCart.setOnClickListener(this);
         btn_orderNow.setOnClickListener(this);
     }
+    private void showData() {
+
+        final Bundle b = getIntent().getExtras();
+        Pid=b.getInt("id");
+        Pname=b.getString("Name");
+        Pprice=b.getString("Price");
+        Pimage=b.getString("Image");
+        final int id = b.getInt("id");
+        name.setText(Pname);
+        company.setText(b.getString("Company"));
+        battery.setText("Battery");
+        memory.setText(b.getString("Memory"));
+        processor.setText(b.getString("Processor"));
+        frnt.setText(b.getString("Front"));
+        bck.setText(b.getString("Back"));
+        os.setText(b.getString("Os"));
+        price.setText(Pprice);
+        Picasso.get().load(Pimage).into(img_product);
+    }
+
 
     private void setSpinner() {
         List<Integer> categories = new ArrayList<>();
